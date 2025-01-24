1974a3225faad873c8f5c9c3336c9d9b7cc40fbc
Jannik S. <jannik.streidl@icloud.com>
2025-01-24 17:18:05 +0100

    Update serve.py
    Signed-off-by: Jannik S. <jannik.streidl@icloud.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -61,13 +61,13 @@ class VLLMDeployment:
             model_config = await self.engine.get_model_config()
             # Determine the name of the served model for the OpenAI client.
             if self.engine_args.served_model_name is not None:
-                served_model_names = self.engine_args.served_model_name
+                served_model_name = self.engine_args.served_model_name
             else:
-                served_model_names = [self.engine_args.model]
+                served_model_name = [self.engine_args.model]
             self.openai_serving_chat = OpenAIServingChat(
                 self.engine,
                 model_config,
-                served_model_names=served_model_names,
+                served_model_name=served_model_name,
                 response_role=self.response_role,
                 lora_modules=self.lora_modules,
                 chat_template=self.chat_template,
