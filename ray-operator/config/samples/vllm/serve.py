e8e18f0058da32322d04066558feb1b96ee75f2e
Jannik S. <jannik.streidl@icloud.com>
2025-02-11 10:01:09 +0100

    Update serve.py
    Signed-off-by: Jannik S. <jannik.streidl@icloud.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -121,7 +121,5 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     )
 
 
-
-
 model = build_app(
     {"model": os.environ['MODEL_ID'], "gpu-memory-utilization": os.environ['GPU_MEMORY_UTILIZATION'], "download-dir": os.environ['DOWNLOAD_DIR'], "max-model-len": os.environ['MAX_MODEL_LEN'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
